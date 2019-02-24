36a20d23bf1e8e137e5e895f4341f488c0b87c13
Riley16 <dashags123456789@gmail.com>
2019-02-23 19:43:25 -0800

    SCOT working without redundancy removal and without set cover, toy problem from SCOT paper generating correct BEC constraints with discounted feature counts

--- SCOT.py
+++ SCOT.py
@@ -20,25 +20,30 @@ def SCOT(mdp, s_start, w):
     # compute optimal policy pi_opt
     _, teacher_pol = value_iteration(mdp)
     print(teacher_pol)
+    mu, mu_sa = get_feature_counts(mdp, teacher_pol)
     teacher_pol = det2stoch_policy(teacher_pol, mdp.nS, mdp.nA)
-
 
     # NOT NEEDED FOR NOW, USE ALTERNATIVE ANALYTICAL MATRIX COMPUTATION FROM APPENDIX B, BROWN AND NIEKUM (2019)
     # compute expected feature counts mu[s][a] under optimal policy
+
 
     # compute BEC of teacher as list of vectors defining halfspaces of linear reward
     # function parameters implied by teacher's policy
     BEC = np.empty((mdp.nS*mdp.nA, w.shape[0]))
 
     # get features for all MDP states
-    phi_s = np.array([list(mdp.s_features[s]) for s in range(mdp.nS)])
+    phi_s = np.array([list(mdp.s_features[s]) for s in range(mdp.nS)]).astype(float)
 
-    print(phi_s)
     T_pi = mdp.get_pol_trans(teacher_pol)
     for a in range(mdp.nA):
         pol_a = det2stoch_policy(np.full(mdp.nS, a), mdp.nS, mdp.nA)
         T_a = mdp.get_pol_trans(pol_a)
-        BEC[a*mdp.nS:(a+1)*mdp.nS] = (T_pi - T_a)@np.linalg.inv(np.eye(mdp.nS) - mdp.gamma*T_pi)@phi_s
+        # BEC[a*mdp.nS:(a+1)*mdp.nS] = (T_pi - T_a)@np.linalg.inv(np.eye(mdp.nS) - mdp.gamma*T_pi)@phi_s
+        BEC[a*mdp.nS:(a+1)*mdp.nS] = mu - mu_sa[:, a]
+        # test0 = T_pi - T_a
+        # test1 = np.linalg.inv(np.eye(mdp.nS) - mdp.gamma*T_pi)
+        # test2 = test0@test1
+        # test = np.ones((mdp.nS, mdp.nS))@phi_s
 
     # remove all trivial (all zero) constraints
     triv_i = []
@@ -59,11 +64,15 @@ def SCOT(mdp, s_start, w):
                 triv_i.add(j)
     BEC = np.delete(BEC, list(triv_i), 0)
 
+    tmp = BEC[0:2]
+    BEC[0:2] = BEC[2:4]
+    BEC[2:4] = tmp
+
     # remove redundant half-space constraints with linear programming
     bounds = tuple([(None, None) for _ in range(w.shape[0])])
     for i in range(BEC.shape[0] - 1, -1, -1):
         c = -BEC[i]
-        A = -np.delete(BEC, i, 0)
+        A = np.delete(BEC, i, 0)
         b = np.zeros(A.shape[0])
         res = linprog(c, A_ub=A, b_ub=b, bounds=bounds, options={"disp": True})
         # DOES THIS MAKE SENSE? DOES AN UNCONSTRAINED PROBLEM NECESSARILY INDICATE A PROBLEM, OR JUST THAT TOO MANY CONSTRAINTS HAVE BEEN REMOVED?
@@ -71,7 +80,7 @@ def SCOT(mdp, s_start, w):
         #     print("Removal of linear redundancies in teacher BEC unsuccessful.")
         #     exit()
         if res.fun <= 0 and not res.status:
-            BEC = -A
+            BEC = A
 
     print(BEC)
 
--- util_algo.py
+++ util_algo.py
@@ -40,23 +40,18 @@ def value_iteration(mdp, tol=1e-3):
 
         # NEED TO ACCOUNT FOR TERMINAL STATES? NIEKUM IS INCLUDING TERMINAL AND INITIAL REWARDS
         for s in range(nS):
-            policy[s] = max([(mdp.reward(s) + sum([mdp.P[s, a, succ] * gamma * value_function_old[succ] * float(not mdp.is_terminal(s))
+            policy[s] = max([(mdp.reward(s) + gamma * sum([mdp.P[s, a, succ] * value_function_old[succ] * float(not mdp.is_terminal(s))
                                    for succ in range(nS)]), a) for a in range(nA)])[1]
 
-            value_function[s] = mdp.reward(s) + sum([mdp.P[s, int(policy[s]), succ] * gamma * value_function_old[succ] * float(not mdp.is_terminal(s))
+            value_function[s] = mdp.reward(s) + gamma * sum([mdp.P[s, int(policy[s]), succ] * value_function_old[succ] * float(not mdp.is_terminal(s))
                                      for succ in range(nS)])
-            # policy[s] = max([(sum([mdp.P[s, a, succ] * (mdp.reward(succ) + gamma * value_function_old[succ]) * float(not mdp.is_terminal(s))
-            #                        for succ in range(nS)]), a) for a in range(nA)])[1]
-            #
-            # value_function[s] = sum([mdp.P[s, int(policy[s]), succ] * (mdp.reward(succ) + gamma * value_function_old[succ]) * float(not mdp.is_terminal(s))
-            #                          for succ in range(nS)])
 
         eps = max(np.absolute(value_function - value_function_old))
     print('VI iterations to convergence: %d' % k)
-    print('value function:')
-    print(value_function)
-    print('policy')
-    print(policy)
+    # print('value function:')
+    # print(value_function)
+    # print('policy')
+    # print(policy)
 
     # print(P)
     print('')
@@ -64,9 +59,43 @@ def value_iteration(mdp, tol=1e-3):
     return value_function, policy
 
 
-# compute expected feature counts mu[s][a] under optimal policy
-def get_feature_counts():
-    pass
+# compute expected  linear feature counts mu[s][a] under optimal policy
+# USE DETERMINISTIC POLICIES (AS INPUTS) FOR NOW
+def get_feature_counts(mdp, policy, tol=1e-6):
+    nS = mdp.nS
+    nA = mdp.nA
+    gamma = mdp.gamma
+    value_function = np.zeros(nS)
+    # discounted expected feature counts under given policy for state s
+    mu = np.zeros((nS, mdp.weights.shape[0]), dtype=float)
+    # discounted expected feature counts mu_sa[s, a] after taking action a from state s
+    # and then following the given policy
+    mu_sa = np.zeros((nS, nA, mdp.weights.shape[0]), dtype=float)
+
+    k = 0
+    eps = tol + 1
+    while eps > tol and k < 100:
+        k = k + 1
+        mu_old = np.copy(mu)
+        for s in range(nS):
+            mu[s] = mdp.s_features[s] + \
+                    gamma * sum([mdp.P[s, int(policy[s]), succ] * mu_old[succ] *
+                                float(not mdp.is_terminal(s)) for succ in range(nS)])
+
+        eps = np.max(np.absolute(mu - mu_old))
+
+    for s in range(nS):
+        for a in range(nA):
+            mu_sa[s, a] = mdp.s_features[s] + \
+                       gamma * sum([mdp.P[s, a, succ] * mu[succ] *
+                                    float(not mdp.is_terminal(s)) for succ in range(nS)])
+
+    print('Bellman iterations to convergence: %d' % k)
+
+    # print(P)
+    print('')
+
+    return mu, mu_sa
 
 # equivalent BEC formulation, Ng and Russell, 2000
 def get_trans_mat(MDP, pol):
