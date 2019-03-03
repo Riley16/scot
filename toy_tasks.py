d7b223dbc23a361de5e8b8f392c261484d8594de
Sarah Radzihovsky <sradzi13@stanford.edu>
2019-03-03 08:23:22 -0800

    running successfully now

--- toy_tasks.py
+++ toy_tasks.py
@@ -3,25 +3,17 @@ import SCOT as scot
 from env import Grid
 from typing import List
 
-Rewards = [1, -1] # for w_white, w_gray
-
 
 gamma = 0.9
-
 
 # Task 1: Cooridor will be one long strip of positively rewarded tiles and a terminal state at the end
 cooridor_width = 6
 cooridor_height = 3
-#cooridor = np.zeros((cooridor_height, cooridor_width))
-#cooridor[0,:] = 1
-#cooridor[2,:] = 1
-#cooridor[1, -1] = 2 # terminal state
-#print(cooridor, "\n")
 squares = []
 for i in range(cooridor_width):
     new_square = [int(cooridor_height/2), i]
     squares.append(new_square)
-
+print(squares)
 
 
 features_sq = [
@@ -30,24 +22,24 @@ features_sq = [
         'reward': -1,
         'squares': squares #[[0, 0], [0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [0, 7], [0, 8], [0, 9],
                    # [2, 0], [2, 1], [2, 2], [2, 3], [2, 4], [2, 5], [2, 6], [2, 7], [2, 8], [2, 9]]
+    },
+    {
+        'color': "red",
+        'reward': 10,
+        'squares': [[int(cooridor_height/2), cooridor_width - 1]] #[[0, 0], [0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [0, 7], [0, 8], [0, 9],
+                   # [2, 0], [2, 1], [2, 2], [2, 3], [2, 4], [2, 5], [2, 6], [2, 7], [2, 8], [2, 9]]
     }
 ]
 
 
-cooridor = Grid(cooridor_height, cooridor_width, gamma, white_r=1, features_sq=features_sq, start_corner=False)
+cooridor = Grid(cooridor_height, cooridor_width, gamma, white_r=0, features_sq=features_sq,
+                start_corner=False, end_pos=(int(cooridor_height/2), cooridor_width - 1))
 
-output = scot.SCOT(cooridor, None, cooridor.weights)
-print(output)
-exit(0)
-ground_truth = []
-
-
-
-#cooridor_soln = [(Grid.grid_to_state(1, cooridor_len - 2), 2,
- #                 Grid.reward(Grid.grid_to_state(1, cooridor_len - 1)), Grid.grid_to_state(1, cooridor_len - 1))]
-#print(cooridor_soln)
-
-
+output_cooridor = scot.SCOT(cooridor, None, cooridor.weights)
+print(output_cooridor)
+end_state = cooridor.grid_to_state((int(cooridor_height/2), cooridor_width - 2))
+ground_truth = [(cooridor.grid_to_state((int(cooridor_height/2), cooridor_width - 2)), 3,
+                 cooridor.reward(end_state), end_state)]
 
 
 # Task 2:
@@ -72,12 +64,19 @@ for row in range(vert_start, vert_end):
     for col in range(horz_start, horz_end):
         new_square = [row, col]
         squares.append(new_square)
-
+print(squares)
+exit(0)
 features_sq = [
     {
         'color': "grey",
         'reward': -1,
         'squares': squares
+    },
+    {
+        'color': "red",
+        'reward': 10,
+        'squares': [[int(cooridor_height/2), cooridor_width - 1]] #[[0, 0], [0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [0, 7], [0, 8], [0, 9],
+                   # [2, 0], [2, 1], [2, 2], [2, 3], [2, 4], [2, 5], [2, 6], [2, 7], [2, 8], [2, 9]]
     }
 ]
 
