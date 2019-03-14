4f3cde02c5a8210c6d21befc2d2d048bee6cb2e2
Riley16 <dashags123456789@gmail.com>
2019-03-14 16:11:11 -0700

    added max likelihood IRL metrics to comments in main.py

--- main.py
+++ main.py
@@ -67,6 +67,45 @@ def main():
           avg_num_trajs, avg_traj_lens, var_num_trajs, var_traj_lens)
     print("avg_time, var_time\n", avg_time, var_time)
 
+    """
+        print("{} grid environment:".format(test.__class__.__name__))
+    test.env.render()
+
+    np.random.seed(2)
+    trajs = SCOT(test.env, None, test.env.weights, verbose=True)
+    # student's inferred reward function from the trajectories from SCOT
+    r_weights = maxLikelihoodIRL(trajs, test.env, step_size=0.1, eps=1.0e-03, max_steps=1000, verbose=True) 
+    print(test.env.weights)
+    print(r_weights)
+    values_MLIRL, policy_MLIRL = value_iteration(mdp=test.env, r_weights=r_weights) # student's policy and value function under student's reward funct
+    values_MLIRL = value_iteration(mdp=test.env, policy=policy_MLIRL) # value of student's policy under teacher's reward funct (true)
+    values_opt, policy_opt = value_iteration(mdp=test.env) # optimal value and policy under teacher's reward funct (true)
+    print(policy_MLIRL)
+    print(policy_opt)
+    policy_similarity = np.sum(policy_MLIRL == policy_opt)/policy_MLIRL.shape[0]
+
+    # policy similarity
+    print("Policy similarity:")
+    print(policy_similarity)
+    print(values_MLIRL)
+    print(values_opt)
+
+    # value gain
+    # FOR NOW, THE START STATE DISTRIBUTION START_DIST DOES NOT HAVE AN ELEMENT FOR THE STATE nS
+    total_value_opt = np.dot(test.env.start_dist, values_opt[:-1])
+    print("optimal expected value")
+    print(total_value_opt)
+
+    total_value_MLIRL = np.dot(test.env.start_dist, values_MLIRL[:-1])
+    print("MLIRL expected value")
+    print(total_value_MLIRL)
+
+    value_gain_MLIRL = total_value_MLIRL/total_value_opt
+    print("Value gain of MLIRL")
+    print(value_gain_MLIRL)
+
+    """
+
 
 if __name__ == "__main__":
     main()
