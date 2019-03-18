1b0ff5dbe5f42cffaa267d9fc1fb031189d07a31
Sarah Radzihovsky <sradzi13@stanford.edu>
2019-03-18 11:58:18 -0700

    added testing code of PI

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from tests import *
 from algorithms.scot import scot
 from algorithms.value_iteration import value_iteration
 from algorithms.max_likelihood_irl import max_likelihood_irl
+from algorithms.policy_iteration import policy_iteration
+from algorithms.policy_evaluation import temporal_difference, every_visit_monte_carlo, first_visit_monte_carlo
 
 def get_env():
     parser = argparse.ArgumentParser()
@@ -47,6 +49,7 @@ def main():
 
     values_MLIRL, policy_MLIRL = value_iteration(mdp=test.env, r_weights=r_weights) # student's policy and value function under student's reward funct
     values_MLIRL, _ = value_iteration(mdp=test.env, policy=policy_MLIRL) # value of student's policy under teacher's reward funct (true)
+
     values_opt, policy_opt = value_iteration(mdp=test.env) # optimal value and policy under teacher's reward funct (true)
     policy_similarity = np.sum(policy_MLIRL == policy_opt)/policy_MLIRL.shape[0]
 
@@ -61,6 +64,35 @@ def main():
 
     value_gain_MLIRL = total_value_MLIRL/total_value_opt
     print("Value gain of Max Likelihood IRL: {}".format(value_gain_MLIRL))
+
+
+    ################################
+
+    est_values_PI, policy_PI = policy_iteration(
+        test.env, test.agent, every_visit_monte_carlo, kwargs={'n_eps': 50, 'eps_len': 10})
+    values_PI, _ = value_iteration(mdp=test.env, policy=policy_PI) # value of student's PI policy under teacher's reward funct (true)
+
+    policy_similarity = np.sum(policy_PI == policy_opt) / policy_PI.shape[0]
+
+    print("Policy similarity: {}".format(policy_similarity))
+
+    # FOR NOW, THE START STATE DISTRIBUTION START_DIST DOES NOT HAVE AN ELEMENT FOR THE STATE nS
+    total_value_opt = np.dot(test.env.start_dist, values_opt)
+    print("Optimal expected value: {}".format(total_value_opt))
+
+    total_value_PI = np.dot(test.env.start_dist, values_PI)
+    print("True PI expected value: {}".format(total_value_PI))
+
+    value_gain_PI = total_value_PI / total_value_opt
+    print("Value gain of true PI: {}".format(value_gain_PI))
+
+    total_value_est_PI = np.dot(test.env.start_dist, est_values_PI)
+    print("Estimated PI expected value: {}".format(total_value_est_PI))
+
+    value_gain_est_PI = total_value_est_PI / total_value_opt
+    print("Value gain of Est PI: {}".format(value_gain_est_PI))
+
+
 
 
 if __name__ == "__main__":
