9f58d477c3d5179d04f39d6bb3c830efc9a1aea3
Sarah Radzihovsky <sradzi13@stanford.edu>
2019-03-18 12:06:33 -0700

    some test code refactoring

--- main.py
+++ main.py
@@ -31,46 +31,11 @@ def get_env():
     return test
 
 
-def main():
-    seed = 2
-    np.random.seed(seed)
-    test = get_env()  # default BrownNiekum()
-    test.env.render()
-    # for i in range(50, 100):
-    #     np.random.seed(i)
-    #     test = get_env()  # default BrownNiekum()
-    #     print("i")
-    #     print(i)
-    #     trajs = scot(test.env, test.env.weights, seed=i+1, verbose=False)
-
-    trajs = scot(test.env, test.env.weights, seed=seed, verbose=False)
-    # student's inferred reward function from the trajectories from SCOT
-    r_weights = max_likelihood_irl(trajs, test.env, step_size=0.2, eps=1.0e-03, max_steps=1000, verbose=True)
-
-    values_MLIRL, policy_MLIRL = value_iteration(mdp=test.env, r_weights=r_weights) # student's policy and value function under student's reward funct
-    values_MLIRL, _ = value_iteration(mdp=test.env, policy=policy_MLIRL) # value of student's policy under teacher's reward funct (true)
-
-    values_opt, policy_opt = value_iteration(mdp=test.env) # optimal value and policy under teacher's reward funct (true)
-    policy_similarity = np.sum(policy_MLIRL == policy_opt)/policy_MLIRL.shape[0]
-
-    print("Policy similarity: {}".format(policy_similarity))
-
-    # FOR NOW, THE START STATE DISTRIBUTION START_DIST DOES NOT HAVE AN ELEMENT FOR THE STATE nS
-    total_value_opt = np.dot(test.env.start_dist, values_opt)
-    print("Optimal expected value: {}".format(total_value_opt))
-
-    total_value_MLIRL = np.dot(test.env.start_dist, values_MLIRL)
-    print("Max Likelihood IRL expected value: {}".format(total_value_MLIRL))
-
-    value_gain_MLIRL = total_value_MLIRL/total_value_opt
-    print("Value gain of Max Likelihood IRL: {}".format(value_gain_MLIRL))
-
-
-    ################################
-
+def test_PI(test, policy_opt, values_opt):
     est_values_PI, policy_PI = policy_iteration(
         test.env, test.agent, every_visit_monte_carlo, kwargs={'n_eps': 50, 'eps_len': 10})
-    values_PI, _ = value_iteration(mdp=test.env, policy=policy_PI) # value of student's PI policy under teacher's reward funct (true)
+    values_PI, _ = value_iteration(mdp=test.env,
+                                   policy=policy_PI)  # value of student's PI policy under teacher's reward funct (true)
 
     policy_similarity = np.sum(policy_PI == policy_opt) / policy_PI.shape[0]
 
@@ -91,6 +56,51 @@ def main():
 
     value_gain_est_PI = total_value_est_PI / total_value_opt
     print("Value gain of Est PI: {}".format(value_gain_est_PI))
+
+def test_scot(test, policy_opt, values_opt, seed):
+    trajs = scot(test.env, test.env.weights, seed=seed, verbose=False)
+    # student's inferred reward function from the trajectories from SCOT
+    r_weights = max_likelihood_irl(trajs, test.env, step_size=0.2, eps=1.0e-03, max_steps=1000, verbose=True)
+
+    values_MLIRL, policy_MLIRL = value_iteration(mdp=test.env,
+                                                 r_weights=r_weights)  # student's policy and value function under student's reward funct
+    values_MLIRL, _ = value_iteration(mdp=test.env,
+                                      policy=policy_MLIRL)  # value of student's policy under teacher's reward funct (true)
+
+    policy_similarity = np.sum(policy_MLIRL == policy_opt) / policy_MLIRL.shape[0]
+
+    print("Policy similarity: {}".format(policy_similarity))
+
+    # FOR NOW, THE START STATE DISTRIBUTION START_DIST DOES NOT HAVE AN ELEMENT FOR THE STATE nS
+    total_value_opt = np.dot(test.env.start_dist, values_opt)
+    print("Optimal expected value: {}".format(total_value_opt))
+
+    total_value_MLIRL = np.dot(test.env.start_dist, values_MLIRL)
+    print("Max Likelihood IRL expected value: {}".format(total_value_MLIRL))
+
+    value_gain_MLIRL = total_value_MLIRL / total_value_opt
+    print("Value gain of Max Likelihood IRL: {}".format(value_gain_MLIRL))
+
+
+def main():
+    seed = 2
+    np.random.seed(seed)
+    test = get_env()  # default BrownNiekum()
+    test.env.render()
+    # for i in range(50, 100):
+    #     np.random.seed(i)
+    #     test = get_env()  # default BrownNiekum()
+    #     print("i")
+    #     print(i)
+    #     trajs = scot(test.env, test.env.weights, seed=i+1, verbose=False)
+
+    values_opt, policy_opt = value_iteration(mdp=test.env) # optimal value and policy under teacher's reward funct (true)
+
+    test_scot(test, policy_opt, values_opt, seed)
+
+
+    test_PI(test, policy_opt, values_opt)
+
 
 
 
